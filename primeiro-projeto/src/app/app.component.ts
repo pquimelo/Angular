2eab2c62335d59f22d83f8e3104b33c432efdc3d
Pedro Juliano Quimelo <88559822+pquimelo@users.noreply.github.com>
2023-01-17 12:49:38 -0300

    ngOnChange
    o ngOnChange so funciona com o  decorator @input()

--- primeiro-projeto/src/app/app.component.ts
+++ primeiro-projeto/src/app/app.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-template: '1<router-outlet></router-outlet>',
+template:`
+<app-title title="Olá Mundo"></app-title>
+1<router-outlet></router-outlet>` ,
 })
 export class AppComponent implements OnInit{
  constructor(){}
