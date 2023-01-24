3fc75ebbea1fed8a34d30541ba3e8f7ced302c79
Pedro Juliano Quimelo <88559822+pquimelo@users.noreply.github.com>
2023-01-24 13:06:52 -0300

    secao_4
    interpolation,Event Binding, property Binding

--- primeiro-projeto/src/app/app.component.ts
+++ primeiro-projeto/src/app/app.component.ts
@@ -1,47 +1,22 @@
-import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnInit } from '@angular/core';
+import {  Component,  OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
 
   template: `
-{{valor}}
-<app-title *ngIf="destruir"></app-title>
-<br>
-<button (click)="destruirComponent()">Destruir Componente</button>
+  <app-data-binding></app-data-binding>
 <router-outlet></router-outlet>`,
 })
-export class AppComponent implements OnInit, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked {
-  public valor: number = 1;
-  public destruir: boolean = true;
+export class AppComponent implements OnInit {
+
 
   constructor() { }
 
-  public adicionar(): number {
-    return this.valor += 1;
-
-  }
+  
   ngOnInit(): void {
     setTimeout(() => {
       console.log('Pedro');
 
     }, 5000);
-  }
-  ngDoCheck(): void {
-    console.log('ngDoCheck');
-  }
-  ngAfterContentInit(): void {
-    console.log('ngAfterContentInit');
-  }
-  ngAfterViewInit(): void {
-    console.log('ngAfterViewInit');
-  }
-  ngAfterContentChecked(): void {
-    console.log('ngAfterContentChecked');
-  }
-  ngAfterViewChecked(): void {
-    console.log('ngAfterViewChecked');
-  }
-  public destruirComponent() {
-    this.destruir = false;
   }
 }
